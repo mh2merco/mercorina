46af8711b91d67a7e3eb13a9116c06566a13c65a
Rina301105 <mariamadalandaba2005@gmail.com>
2026-06-27 16:32:05 +0000

    connexion avec la base de donnée

--- src/dao/ConnectionDB.java
+++ src/dao/ConnectionDB.java
@@ -5,14 +5,18 @@ import java.sql.DriverManager;
 
 public class ConnectionDB {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/gestion_etudiant";
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/gestion_etudiant";
 
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
             return DriverManager.getConnection(URL, USER, PASSWORD);
+
         } catch (Exception e) {
             System.out.println("Erreur connexion DB : " + e.getMessage());
             return null;
